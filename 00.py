56143865a1ad2d70f8f89ac2834e7c74e63562c3
aleaases <aleaases@gmail.com>
2024-09-18 16:46:17 -0300

    actualizo 00

---
+++ 00.py
@@ -0,0 +1,33 @@
+# esto es un comentario
+# esto tambien es un comentario
+
+# son los dos iguales
+print ("hola python")
+print ('hola python')
+
+""" 
+esto tambien es un cometario en python para 
+practicar  
+"""
+# son los dos iguales
+print (type ("hola mundo") )
+print (type("555155"))
+
+
+print(2 + 3)             # addition(+)
+print(3 - 1)             # subtraction(-)
+print(2 * 3)             # multiplication(*)
+print(3 / 2)             # division(/)
+print(3 ** 2)            # exponential(**)
+print(3 % 2)             # modulus(%)
+print(3 // 2)            # Floor division operator(//)
+
+# Checking data types
+print(type(10))          # Int
+print(type(3.14))        # Float
+print(type(1 + 3j))      # Complex number
+print(type('Asabeneh'))  # String
+print(type([1, 2, 3]))   # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
